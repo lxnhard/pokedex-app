8508cbaf9bbd9f8c9a120ac26aa84f9c95382c4c
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-06-09 17:17:44 +0200

    basic project outline

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+alert('Hello world');
+
+// Just playing around to see what I remember from years ago
+let favoriteFood = ['Lasagna', 'Pizza', 'Pig Testicles', 'Glue', 'Sun light'];
+for (let i = 0; i < favoriteFood.length-1; i++) {
+  document.write(favoriteFood[i] + ', ');
+};
+document.write(favoriteFood[favoriteFood.length-1]);
