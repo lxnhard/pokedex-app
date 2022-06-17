c76acac1e255dbdadaea0f672079e2cfab040b8f
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-06-17 18:24:30 +0200

    IIFE for pokemonRepository with functions add and getAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-let pokemonList = [
-  {name: 'Arbok', height: 3.5, types: ['poison']},
-  {name: 'Poliwrath', height: 1.3, types: ['water', 'fighting']},
-  {name: 'Armaldo', height: 1.5, types: ['bug','rock']},
-  {name: 'Charmander', height: 0.6, types: ['fire']}
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Arbok', height: 3.5, types: ['poison']},
+    {name: 'Poliwrath', height: 1.3, types: ['water', 'fighting']},
+    {name: 'Armaldo', height: 1.5, types: ['bug','rock']},
+    {name: 'Charmander', height: 0.6, types: ['fire']}
+  ];
+//function to retrieve Pokemon
+  function getAll() {
+    return pokemonList;
+  }
+//function to add new Pokemon
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll,
+    add
+  }
+})();
 
 //Print all objects in pokemonList (name, height and compliment for tall boys)
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write("<p>"+pokemon.name+" (height: "+pokemon.height+")");
   (pokemon.height > 2) ? (document.write(" – "+"That's a tall boy!</p>")) : (document.write("</p>"));
 });
