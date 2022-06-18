69dd74120fe0bc3e9357bc1f9ffc7aa91c6ac4d0
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-06-18 13:14:12 +0200

    add public function findName

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,33 @@ let pokemonRepository = (function() {
     {name: 'Armaldo', height: 1.5, types: ['bug','rock']},
     {name: 'Charmander', height: 0.6, types: ['fire']}
   ];
-//function to retrieve Pokemon
+  //function to retrieve all Pokemon
   function getAll() {
     return pokemonList;
   }
-//function to add new Pokemon
+  //function to add new Pokemon
   function add(item) {
     pokemonList.push(item);
   }
-
+  //function to filter for Pokemon with specific name
+  function findName(name) {
+    result = pokemonList.filter(pokemon => pokemon.name === name);
+    if (result.length > 0) {
+      return result[0];
+    } else {
+      console.log('The Pokémon you called is currently not available.');
+    }
+  }
+  //return public functions
   return {
     getAll,
-    add
+    add,
+    findName
   }
 })();
 
 //Print all objects in pokemonList (name, height and compliment for tall boys)
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write("<p>"+pokemon.name+" (height: "+pokemon.height+")");
-  (pokemon.height > 2) ? (document.write(" – "+"That's a tall boy!</p>")) : (document.write("</p>"));
+  document.write('<p>'+pokemon.name+' (height: '+pokemon.height+')');
+  (pokemon.height > 2) ? (document.write(" – That's a tall boy!</p>")) : (document.write('</p>'));
 });
