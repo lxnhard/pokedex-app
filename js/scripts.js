8cd4901b8365d393c48ce37c32754d7b5f01b606
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-07-05 23:04:13 +0200

    Add back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -223,3 +223,20 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$("#back-to-top").fadeIn();
+			} else {
+				$("#back-to-top").fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$("#back-to-top").click(function () {
+			$("body,html").animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
